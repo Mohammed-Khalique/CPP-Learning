102987aecb47ff0da8080f964520eb391d42daaf
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-04-30 20:03:15 +0100

    chore: added an add method, updated main method

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int add(int a, int b);
+
 int main()
 {
-    cout << "Did this change the console?" << endl;
+
+    int firstNumber, secondNumber;
+    cout << "Enter a number: " << endl;
+    cin >> firstNumber;
+    cout << "You entered: " << firstNumber << endl;
+
+    cout << "Enter a second number: " << endl;
+    cin >> secondNumber;
+    cout << "You entered " << secondNumber << endl;
+
+    cout << "Apparently the calculation two adding them is " << add(firstNumber, secondNumber) << endl;
+
     return 0;
 }
+
+
+
+int add(int a, int b){
+    return a + b;
+}
+
+
