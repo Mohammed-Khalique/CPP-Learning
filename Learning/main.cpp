10b17a6228e7d7354e9245f9860793137a3f79c9
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-06 22:04:26 +0100

    chore: while-loops

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -3,23 +3,14 @@
 using namespace std;
 
 
-string dayofweek(int daynum){
-string dayname;
-switch(daynum){
-case 0: dayname = "Sunday"; break;
-case 1: dayname = "Monday"; break;
-case 2: dayname = "Tuesday"; break;
-case 3: dayname = "Wednesday"; break;
-case 4: dayname = "Thursday"; break;
-case 5: dayname = "Friday"; break;
-case 6: dayname = "Saturday"; break;
-default: dayname = "Invalid day number";
-}
-   return dayname;
-}
+
 int main()
 {
-cout << dayofweek(0);
+int index = 1;
+while(index <= 100){
+    cout << index << endl;
+    index++;
+}
 
    return 0;
 }
