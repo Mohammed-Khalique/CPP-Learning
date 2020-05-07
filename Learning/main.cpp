e1750a329733dc9fd3c4c7f7b52ec7eb64a7a540
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-08 00:02:56 +0100

    chore: Guessing-Game

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -6,11 +6,24 @@ using namespace std;
 
 int main()
 {
-int index = 1;
-do{
-    cout << index << endl;
-    index++;
-}while(index <= 100);
+    cout <<"Pick A Number Between 1-100\n--------------"<< endl;
+    int secretnum = 69;
+    int guess;
+    int guesscount = 0;
+    int guesslimit = 5;
+
+    bool outofguesses = false;
+
+    while(secretnum != guess && !outofguesses){
+    if(guesscount < guesslimit)
+    {cout << "Enter A Number: ";
+    cin >> guess;
+    guesscount++;}
+    else{outofguesses = true;}
+    }
+    if(outofguesses)
+    {cout << " \nYou Lose";}
+    else{cout << " \nYou Win";}
 
    return 0;
 }
