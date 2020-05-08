42cfc07da825a8d7d20bd859e0175cbe98a47a1b
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-08 23:33:12 +0100

    chore: exponent-function

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -2,14 +2,16 @@
 #include <cmath>
 using namespace std;
 
-
-
+int power(int basenum, int pownum){
+int result = 1;
+for(int i = 0; i < pownum; i++)
+{result = result * basenum;
+}
+return result;
+}
 int main()
 {
-    int nums[] = {46, 69, 360, 82, 419};
-    for(int i = 0; i < 5; i++)
-    {cout << nums[i] << endl;}
-
-   return 0;
+cout << power(6, 9);
+return 0;
 }
 
