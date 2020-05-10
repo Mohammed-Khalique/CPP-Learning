bbbfc0e4db9c1f4a7b45f5d86bc7ae88a47a6dd0
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-10 16:06:13 +0100

    chore: 2d-arrays

--- Learning/main.cpp
+++ Learning/main.cpp
@@ -2,16 +2,14 @@
 #include <cmath>
 using namespace std;
 
-int power(int basenum, int pownum){
-int result = 1;
-for(int i = 0; i < pownum; i++)
-{result = result * basenum;
-}
-return result;
-}
+
 int main()
 {
-cout << power(6, 9);
+    int numbergrid[3][2]={
+        {1, 2},{3, 4},{5, 6}
+    };
+    cout << numbergrid[0][1];
+
 return 0;
 }
 
