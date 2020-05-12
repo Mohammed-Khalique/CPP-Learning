1d78b2d0ad1d91bbfd0299a8d5af48c3349fda09
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-12 19:36:08 +0100

    chore: constructor-functions

--- Learning/classes and objects/main.cpp
+++ Learning/classes and objects/main.cpp
@@ -6,18 +6,17 @@ public:
     string name;
     string models;
     string colour;
+
+    phone(string aname, string amodels, string acolour){
+    name = aname;
+    models = amodels;
+    colour = acolour;
+    }
 };
 int main()
 {
-    phone phone1;
-    phone1.name = "Apple";
-    phone1.models = "IPhone 11 Pro Max";
-    phone1.colour= "red";
-
-    phone phone2;
-    phone2.name = "Samsung";
-    phone2.models = "Samsung S20 Ultra";
-    phone2.colour = "black";
+    phone phone1("Apple","IPhone 11 Pro Max","red");
+    phone phone2("Samsung","Samsung S20 Ultra","black");
 
     cout << phone2.models;
 
