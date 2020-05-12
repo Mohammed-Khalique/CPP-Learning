49613d9252318a29562c92fd15cbffc9e1b91f85
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-13 00:17:27 +0100

    chore: object-functions

--- Learning/classes and objects/main.cpp
+++ Learning/classes and objects/main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 class phone{
 public:
     string name;
     string models;
     string colour;
+    int year;
 
-    phone(string aname, string amodels, string acolour){
+
+    phone(string aname, string amodels, string acolour, int ayear){
     name = aname;
     models = amodels;
     colour = acolour;
-    }
+    year = ayear;}
+
+    bool isnew(){if(year >= 2020){return true;}
+    return false;}
 };
+
 int main()
 {
-    phone phone1("Apple","IPhone 11 Pro Max","red");
-    phone phone2("Samsung","Samsung S20 Ultra","black");
+    phone phone1("Apple", "IPhone 11 Pro Max", "red",2018);
+    phone phone2("Samsung", "Samsung S20 Ultra", "black",2020);
 
-    cout << phone2.models;
+    cout << phone1.isnew();
+
 
     return 0;
 }
