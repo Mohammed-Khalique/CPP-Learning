799444c9c70c88b0fbae0b59663a3cc76e2310ab
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-13 20:26:51 +0100

    chore: getters-and-setters

--- Learning/classes and objects/main.cpp
+++ Learning/classes and objects/main.cpp
@@ -2,29 +2,39 @@
 #include <cmath>
 using namespace std;
 class phone{
+
+private:
+    string colour;
+
 public:
     string name;
     string models;
-    string colour;
     int year;
 
 
     phone(string aname, string amodels, string acolour, int ayear){
     name = aname;
     models = amodels;
-    colour = acolour;
+    setcolour(acolour);
     year = ayear;}
 
-    bool isnew(){if(year >= 2020){return true;}
-    return false;}
+    void setcolour(string acolour){
+    if(acolour == "silver" || acolour == "black" || acolour == "grey" || acolour == "invalid")
+       {colour = acolour;}
+       else {colour = "invalid";}
+    }
+    string getcolour(){return colour;}
+
 };
 
 int main()
 {
-    phone phone1("Apple", "IPhone 11 Pro Max", "red",2018);
+    phone phone1("Apple", "IPhone 11 Pro Max", "silver",2018);
     phone phone2("Samsung", "Samsung S20 Ultra", "black",2020);
 
-    cout << phone1.isnew();
+    //phone1.setcolour("yellow");
+
+    cout << phone1.getcolour();
 
 
     return 0;
