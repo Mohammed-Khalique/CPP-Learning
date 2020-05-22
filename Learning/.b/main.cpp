10bb87a473a48c05b1621e07d4e31f6efe142df1
Mohammed-Khalique <mohammedkhalique646@gmail.com>
2020-05-22 18:50:22 +0100

    chore: password-program-v3

--- Learning/.b/main.cpp
+++ Learning/.b/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
-void question(){
+int main(){
 
     string yesorno = "y";
 
@@ -12,37 +14,23 @@ void question(){
     string pw;
     int length;
     cout << "Please Enter A Password Within 1-10:";
-    //getline(cin, pw);
     cin >> pw;
     length = pw.size();
+    bool gg = false;
+    if (std::string::npos != pw.find_first_of("0123456789"))
+    {gg = true;}
+    else{gg = false;}
 
     if( length > 10){cout << "This Is Too Long" << endl;}
-    else if(length <= 5){cout << "This Password Is Weak" << endl;}
-    else if(length <= 10){cout << "This Password Is Medium" << endl;}
+    else if(length <= 5 && gg == false){cout << "This Password Is Bad" << endl;}
+    else if(length <= 5 && gg == true){cout << "This Password Is Weak" << endl;}
+    else if(length <= 10 && gg == false){cout << "This Password Is Medium" << endl;}
+    else if(length <= 10 && gg == true){cout << "This Password Is Strong" << endl;}
     else{cout << "invalid";}
 
     cout << "Would You Like To Try Another Password (y/n)" << endl;
     cin >> yesorno;
 
     }
-
-
-
-}
-
-
-
-
-int main()
-{
-    question();
-
-
-
-
-
-
-
-
     return 0;
 }
